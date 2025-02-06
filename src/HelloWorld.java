28faa5bc56c0bd12980a0668eed7c53817873daa
Yahya9870 <56015489+Yahya9870@users.noreply.github.com>
2025-02-05 22:33:35 -0500

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,37 @@
+public class HelloWorld {
+
+    // public is access/visible modifier
+    // class is a keyword used in order to create a class
+    // HelloWorld is the name of the class that can be anything whatever ypu want.
+
+    // Static: Static: No object is required to access the function/method.
+    //So, when you see public static void main(String[] args),
+    // the static keyword ensures that the main method can be called without an object of the class,
+    // and it serves as the entry point for your Java program.
+
+    // void: return type (void means method is not returning any value)
+    // main: name of the main method of java without that we cannot expect a java program.
+
+
+
+
+    //Core engine of Java Program cum entry point of a java program this main method.
+    public static void main(String[] args) {
+
+        System.out.println("Hello Java");
+
+        // System is an inbuilt/predefined class of java in java.lang package.
+        // out is an instance of that class.
+
+        // System.out ()it's for standard output
+        // System.in for standard input
+        // System.err for standard error
+        // print is method of PrintStream class that we can use using instance of the same class in order to display something over the console
+
+    }
+
+
+}
+
+
+
